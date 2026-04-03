b14f8d509d1e8d6da41199fd38c752a1931d7178
Christian Simon <simon@swine.de>
2026-04-03 14:45:36 +0100

    feat(labels): add UTF-8 Accept header to Series API call

--- src/pages/ProfilesExplorerView/infrastructure/series/http/SeriesApiClient.ts
+++ src/pages/ProfilesExplorerView/infrastructure/series/http/SeriesApiClient.ts
@@ -1,3 +1,4 @@
+import { featureToggles } from '@shared/infrastructure/settings/featureToggles';
 import { ProfileMetric } from '@shared/infrastructure/profile-metrics/getProfileMetric';
 
 import { DataSourceProxyClient } from './DataSourceProxyClient';
@@ -27,6 +28,9 @@ export class SeriesApiClient extends DataSourceProxyClient {
         labelNames: ['service_name', '__profile_type__'],
         matchers: [],
       }),
+      headers: featureToggles.pyroscopeUTF8LabelNames
+        ? { accept: 'application/json; allow-utf8-labelnames=true' }
+        : {},
     })
       .then((response) => response.json())
       .then(formatSeriesResponse);
